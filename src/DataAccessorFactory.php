74805d34d3326758e73c4f7ef04a0cea3a89b904
Adam Lutka <adam.lutka@gmail.com>
2024-07-09 15:10:51 +0200

    DataAccessor is not generic

--- src/DataAccessorFactory.php
+++ src/DataAccessorFactory.php
@@ -5,34 +5,18 @@ declare(strict_types=1);
 namespace ScrumWorks\DataAccessor;
 
 use ScrumWorks\DataAccessor\Exception\JsonDecodeException;
-use ScrumWorks\EnumMapping\AbstractEnumMapping;
-use ScrumWorks\EnumMapping\EnumMappingProvider;
-use UnitEnum;
 
-/**
- * @template TEnumMapping of AbstractEnumMapping
- * @template TUnitEnum of UnitEnum
- */
 final readonly class DataAccessorFactory
 {
     /**
-     * @param EnumMappingProvider<TEnumMapping, TUnitEnum> $enumMappingProvider
-     */
-    public function __construct(private EnumMappingProvider $enumMappingProvider)
-    {
-    }
-
-    /**
-     * @return DataAccessor<TEnumMapping, TUnitEnum>
      * @throw JsonDecodeException
      */
     public function createFromData(mixed $data): DataAccessor
     {
-        return new DataAccessor($this, $this->enumMappingProvider, $data);
+        return new DataAccessor($this, $data);
     }
 
     /**
-     * @return DataAccessor<TEnumMapping, TUnitEnum>
      * @throw JsonDecodeException
      */
     public function createFromJson(string $json): DataAccessor
